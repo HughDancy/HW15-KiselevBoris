c31feb7c3763478d273c1056f850ef871937fadc
BorisKiselev <hugh.dancy@icloud.com>
2022-06-19 22:05:28 +0700

    Settings TableViewController

--- HW15-KiselevBoris/Sources/App/AppDelegate.swift
+++ HW15-KiselevBoris/Sources/App/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let firstViewController = ViewController()
+        let firstViewController = TableViewController()
         let firstNavigationController = UINavigationController(rootViewController: firstViewController)
         firstViewController.title = "Настройки"
         firstViewController.navigationItem.hidesSearchBarWhenScrolling = true
